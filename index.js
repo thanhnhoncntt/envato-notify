563b4081336bfed3d925b43d9df6e79ada61bd6d
NhonNguyen <thanhnhon_cntt@hotmail.com>
2022-12-25 22:04:05 +0700

    fix bug

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var fs = require("fs");
+const { promises: fs } = require("fs");
 require("dotenv").config();
 const Envato = require("envato");
 const TelegramBot = require("node-telegram-bot-api");
-const saleLastInFile = require("./sale.json");
 const moment = require("moment");
 const bot = new TelegramBot(process.env.TOKEN_TELEGRAM, { polling: true });
 var CronJob = require("cron").CronJob;
@@ -15,22 +14,31 @@ const client = new Envato.Client({
 });
 
 async function checkSale() {
-  const sales = await client.private.getSales();
-  const saleLastApi = sales[0];
-  if (
-    saleLastInFile &&
-    moment(saleLastApi.sold_at).diff(moment(saleLastInFile.sold_at)) > 0
-  ) {
-    const mess = `Mới bán được item: ${
-      saleLastApi["item"]["name"]
-    } lúc ${saleLastApi.sold_at.toLocaleString()}`;
-    bot.sendMessage(process.env.CHATID, mess);
-    fs.writeFile("sales.json", JSON.stringify(saleLastApi), function (err) {
-      if (err) throw err;
-      console.log("complete");
-    });
+  try {
+    const sales = await client.private.getSales();
+    const saleLastApi = sales[0];
+    const saleLastInFile = JSON.parse(await fs.readFile("sale.json", "utf8"));
+    if (
+      saleLastInFile &&
+      moment(saleLastApi.sold_at).diff(moment(saleLastInFile.sold_at)) > 0
+    ) {
+      const mess = `Mới bán được item: ${
+        saleLastApi["item"]["name"]
+      } lúc ${saleLastApi.sold_at.toLocaleString()}`;
+      bot.sendMessage(process.env.CHATID, mess);
+      await fs.writeFile(
+        "sale.json",
+        JSON.stringify(saleLastApi),
+        function (err) {
+          if (err) throw err;
+          console.log("complete");
+        }
+      );
+    }
+    return true;
+  } catch (error) {
+    console.log("error: " + error);
   }
-  return true;
 }
 
 const job = new CronJob(
